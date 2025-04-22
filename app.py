3594e3bbd6b750a84f444b1cd4c1308619b10058
Sarah Orton <sarahorton25@gmail.com>
2025-04-22 11:22:26 -0500

    Added post feature (have to refresh)

--- app.py
+++ app.py
@@ -13,5 +13,17 @@ def get_posts():
         posts = json.load(file)
     return jsonify(posts)
 
+@app.route('/api/add_post', methods+['POST'])
+def add_post():
+    new_post = request.get_json()
+    with open('data.json', 'r') as file:
+        posts = json.load(file)
+    posts.insert(0, new_post)
+    with open('data.json', 'w') as file:
+        json.dump(posts, file, indent=4)
+
+    return jsonify({"status" : "success"}), 201
+
+
 if __name__ == '__main__':
     app.run(debug=True)
--- static/js/app.js
+++ static/js/app.js
@@ -7,10 +7,22 @@ function renderPost(post) {
     document.getElementById("feed").appendChild(template);
 }
 
-function submitPost() {
+ async function submitPost() {
     const message = document.getElementById("postInput").value;
-    console.log("Would post:", message);
-    alert("Tweet submitted (not really yet)");
+        try{
+            const response =  await fetch("/api/add_post", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({
+                    username,
+                    message,
+                }),
+            }):
+        } catch (error) {
+            console.log("😭 Post failed", error)
+        }
 }
 
 window.onload = async () => {
